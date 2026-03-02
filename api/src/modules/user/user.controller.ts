3663cee5dec8014b0edce63bc84046b1d462fb8b
Edward Garcia <edward.ariola.garcia@gmail.com>
2026-03-02 09:07:16 +0800

    update: try-catch on controller

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -1,30 +1,70 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as userService from "./user.service";
 
-export async function getUsers(req: Request, res: Response) {
-	const users = await userService.getUsers();
-	res.json(users);
+export async function getUsers(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const users = await userService.getUsers();
+		res.json(users);
+	} catch (error) {
+		next(error);
+	}
 }
 
-export async function getUserById(req: Request, res: Response) {
-	const id = Number(req.params.id);
-	const user = await userService.getUserById(id);
-	res.json(user);
+export async function getUserById(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const id = Number(req.params.id);
+		const user = await userService.getUserById(id);
+		res.json(user);
+	} catch (error) {
+		next(error);
+	}
 }
 
-export async function createUser(req: Request, res: Response) {
-	const user = await userService.registerUser(req.body);
-	res.status(201).json(user);
+export async function createUser(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const user = await userService.registerUser(req.body);
+		res.status(201).json(user);
+	} catch (error) {
+		next(error);
+	}
 }
 
-export async function updateUser(req: Request, res: Response) {
-	const id = Number(req.params.id);
-	const user = await userService.patchUser({ id, ...req.body });
-	res.json(user);
+export async function updateUser(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const id = Number(req.params.id);
+		const user = await userService.patchUser({ id, ...req.body });
+		res.json(user);
+	} catch (error) {
+		next(error);
+	}
 }
 
-export async function deleteUser(req: Request, res: Response) {
-	const id = Number(req.params.id);
-	const deletedUser = await userService.deleteUser(id);
-	res.status(200).json(deletedUser);
+export async function deleteUser(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const id = Number(req.params.id);
+		const deletedUser = await userService.deleteUser(id);
+		res.status(200).json(deletedUser);
+	} catch (error) {
+		next(error);
+	}
 }
